d3ab03e6273af2fca465e2ed2e1a6b0353c1437e
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-08-31 09:16:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.02 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.Length <= 1) {
+            return new int[] { -1 };
+        }
+        
+        int minIndex = Array.IndexOf(arr, arr.Min());
+        
+        return arr.Where((val, idx) => idx != minIndex).ToArray();
+    }
+}
