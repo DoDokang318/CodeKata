4d6ea37d4828d29e5b153b85b1fc110bad5678f1
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-09-26 09:09:18 +0900

    [unrated] Title: 콜라 문제, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+  
+        for(int i = 0 ; n >= a;i++ )
+        {               
+             answer+= (n/a)*b;
+             n =(n/a)*b+n%a;        
+            
+           
+        }
+        return answer;
+    
+    }
+}
